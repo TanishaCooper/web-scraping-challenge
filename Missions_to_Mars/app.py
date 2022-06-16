6062ea22924b84b9f0c18282cf0740cdd2b61ae2
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-06-16 08:57:28 -0400

    app.py updated

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,9 +1,6 @@
 # Part 3: Flask
 
 
-from cgitb import html
-import code
-from re import template
 import scrape_mars
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
@@ -16,15 +13,19 @@ mongo = PyMongo(app)
 
 # Define your routes that renders index.html template and finds documents from MongoDB
 @app.route("/")
-def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+def home():
+    data = mongo.db.data.find_one()
+    return render_template("index.html", data=data)
 
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_data = scrape_mars.scrape_all()
-    mars.update({}, mars_data, upsert=True)
+    data = mongo.db.data
+    mars_data = scrape_mars.mars_news(browser)
+    mars_data = scrape_mars.featured_image(browser)
+    mars_data = scrape_mars.mars_facts()
+    mars_data = scrape_mars.mars_hemis(browser)
+    data.update({}, mars_data, upsert=True)
+    
     return redirect('/', code=302)
 
 if __name__== "__main__":
