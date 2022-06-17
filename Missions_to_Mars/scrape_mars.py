8af02a7e30149becbe2ee91a1950cb0135adec97
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-06-17 15:02:52 -0400

    scrape validated

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -76,11 +76,8 @@ def mars_facts():
 
     mars_facts_df.columns=['Description', 'Mars', 'Earth']
     mars_facts_df.set_index('Description', inplace=True)
-    mars_facts_tr_html = mars_facts_df.to_html()
-    mars_facts_tr_html = mars_facts_tr_html.replace('\n', '')
-    mars_facts_tr_html = mars_facts_tr_html.replace("<table border=\"1\" class=\"dataframe\">", "").replace("</table>","").strip()
 
-    return mars_facts_tr_html.to_html()
+    return mars_facts_df.to_html()
 
 
 def mars_hemis(browser):
